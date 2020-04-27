9ea764132604bd4464a08e9a44671420a1a65f5f
voghost <voghost2@gmail.com>
2020-04-27 19:54:58 +0800

    rename 1.twoSum_1.c

---
+++ src/1.twoSum_1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*  *nums-> 数组,  numsSize-> 数组长度,  target-> 要得到的目标, *returnSize 要返回数组的长度(在函数中赋值)  */
+int *twoSum (int *nums,int numsSize,int target,int *returnSize){
+	int i,j;
+	(*returnSize)=2;
+	int *returnArray=(int *)malloc(sizeof(int)*(*returnSize));
+	returnArray[0]=-1;
+	returnArray[1]=-1;
+	
+	for(i=0;i<numsSize;i++){
+		for(j=i;j<numsSize;j++){
+			if(nums[i]+nums[j]==target){
+				returnArray[0]=i;
+				returnArray[1]=j;
+				break;
+			}	
+		}
+	}	
+	return returnArray;
+}
+
+int main(int argc, char *argv[])
+{
+	int nums[4]={2,7,11,15};	
+	int size;
+	int *result=twoSum(nums, 4, 9, &size);
+	for(int i=0;i<size;i++){
+		printf("%d  ",result[i]);
+	}
+	free(result);
+	return 0;
+}
+
