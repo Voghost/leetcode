db4ca0e91fe505f7ada2fd7ac1fd47ff6000a0fc
voghost <voghost2@gmail.com>
2020-04-27 19:11:40 +0800

    init leetcode

---
+++ src/twoSum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int *twoSum (int *nums,int numsSize,int target,int *returnSize){
+	int i,j;
+	for(i=0;i<numsSize;i++){
+		for(j=0;j<numsSize;j++){
+		}
+	}	
+}
+
+int main(int argc, char *argv[])
+{
+	
+	return 0;
+}
+
