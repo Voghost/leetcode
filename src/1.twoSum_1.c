bf40fb50948e118e241960df42fc9a0dd6ab384e
voghost <voghost2@gmail.com>
2020-09-16 19:57:55 +0800

    格式化代码

--- src/1.twoSum_1.c
+++ src/1.twoSum_1.c
@@ -7,34 +7,32 @@
  * */
 
 /*  *nums-> 数组,  numsSize-> 数组长度,  target-> 要得到的目标, *returnSize 要返回数组的长度(在函数中赋值)  */
-int *twoSum (int *nums,int numsSize,int target,int *returnSize){
-	int i,j;
-	(*returnSize)=2;
-	int *returnArray=(int *)malloc(sizeof(int)*(*returnSize));
-	returnArray[0]=-1;
-	returnArray[1]=-1;
-	
-	for(i=0;i<numsSize;i++){
-		for(j=i;j<numsSize;j++){
-			if(nums[i]+nums[j]==target){
-				returnArray[0]=i;
-				returnArray[1]=j;
-				break;
-			}	
-		}
-	}	
-	return returnArray;
+int *twoSum (int *nums, int numsSize, int target, int *returnSize) {
+    int i, j;
+    (*returnSize) = 2;
+    int *returnArray = (int *)malloc(sizeof(int) * (*returnSize));
+    returnArray[0] = -1;
+    returnArray[1] = -1;
+
+    for(i = 0; i < numsSize; i++) {
+        for(j = i; j < numsSize; j++) {
+            if(nums[i] + nums[j] == target) {
+                returnArray[0] = i;
+                returnArray[1] = j;
+                break;
+            }
+        }
+    }
+    return returnArray;
 }
 
-int main(int argc, char *argv[])
-{
-	int nums[4]={2,7,11,15};	/*随机给出的数据*/
-	int size;
-	int *result=twoSum(nums, 4, 9, &size);
-	for(int i=0;i<size;i++){
-		printf("%d  ",result[i]);
-	}
-	free(result);
-	return 0;
+int main(int argc, char *argv[]) {
+    int nums[4] = {2, 7, 11, 15};	/*随机给出的数据*/
+    int size;
+    int *result = twoSum(nums, 4, 9, &size);
+    for(int i = 0; i < size; i++) {
+        printf("%d  ", result[i]);
+    }
+    free(result);
+    return 0;
 }
-
